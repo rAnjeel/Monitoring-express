9ad1da2fda5c11b6411402f87cc2cf4b431300d6
rAnjeel <rabearisonangelo21@gmail.com>
2025-09-16 13:51:59 +0300

    Apply validation on routes

--- backend/src/routes/location.routes.js
+++ backend/src/routes/location.routes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const locationController = require('../controllers/location.controller');
+const validateLocation = require('../middlewares/locationValidation');
 
 router.get('/', locationController.getAll.bind(locationController));
 router.get('/:id', locationController.getById.bind(locationController));
-router.post('/', locationController.create.bind(locationController));
-router.put('/:id', locationController.update.bind(locationController));
+router.post('/', validateLocation, locationController.create.bind(locationController));
+router.put('/:id', validateLocation, locationController.update.bind(locationController));
 router.delete('/:id', locationController.delete.bind(locationController));
 
 module.exports = router;
--- backend/src/routes/typeDevice.routes.js
+++ backend/src/routes/typeDevice.routes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const typeDeviceController = require('../controllers/typeDevice.controller');
+const validateTypeDevice = require('../middlewares/typeDeviceValidation');
 
 router.get('/', typeDeviceController.getAll.bind(typeDeviceController));
 router.get('/:id', typeDeviceController.getById.bind(typeDeviceController));
-router.post('/', typeDeviceController.create.bind(typeDeviceController));
-router.put('/:id', typeDeviceController.update.bind(typeDeviceController));
+router.post('/', validateTypeDevice, typeDeviceController.create.bind(typeDeviceController));
+router.put('/:id', validateTypeDevice, typeDeviceController.update.bind(typeDeviceController));
 router.delete('/:id', typeDeviceController.delete.bind(typeDeviceController));
 
 module.exports = router;
