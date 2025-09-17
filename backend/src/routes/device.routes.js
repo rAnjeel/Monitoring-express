148c29be57ac1343178f9a18057f76209440c378
rAnjeel <rabearisonangelo21@gmail.com>
2025-09-17 11:56:37 +0300

    import device csv

--- backend/src/routes/device.routes.js
+++ backend/src/routes/device.routes.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const multer = require('multer');
+
 const router = express.Router();
 const deviceController = require('../controllers/device.controller');
-const validateDevice = require('../middlewares/device.middleware');
+const upload = multer({ dest: 'uploads/' });
 
 router.get('/', deviceController.getAll.bind(deviceController));
 router.get('/:id', deviceController.getById.bind(deviceController));
-router.post('/', validateDevice, deviceController.create.bind(deviceController));
-router.put('/:id', validateDevice, deviceController.update.bind(deviceController));
+router.post('/', deviceController.create.bind(deviceController));
+router.put('/:id', deviceController.update.bind(deviceController));
 router.delete('/:id', deviceController.delete.bind(deviceController));
+router.post('/import', upload.single('file'), deviceController.importCSV.bind(deviceController));
 
 module.exports = router;
